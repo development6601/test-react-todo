a682dda509d3fe963a4595f0c060fedd7e1d6244
development6601 <development6601@gmail.com>
2025-12-31 17:44:06 +0530

    feat: Update App component to include multiple TodoApp instances and a header
    - Added a header with "Hello World"
    - Rendered multiple instances of the TodoApp component within a div

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import { TodoApp } from './components/TodoApp/TodoApp'
 import './App.css'
 
 function App() {
-  return <TodoApp />
+  return (
+    <div>
+      <h1>Hello World</h1>
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+    </div>
+  )
 }
 
 export default App
