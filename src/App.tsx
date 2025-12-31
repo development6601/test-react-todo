f5ae0c7c8ef8586af0cdef38dd559f2eea88e999
development6601 <development6601@gmail.com>
2025-12-31 17:48:41 +0530

    feat: Add main App component with multiple TodoApp instances
    Created a new App component that renders a greeting and multiple instances of the TodoApp component. This sets up the main structure for the application.

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { TodoApp } from './components/TodoApp/TodoApp'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+         <TodoApp />
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+         <TodoApp />
+       <TodoApp />
+       <TodoApp />
+       <TodoApp />
+    </div>
+  )
+}
+
+export default App
